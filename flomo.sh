22598948635c59e4a7404a62a3cfe47ddae00bce
southpaw-wu <34786853+southpaw-wu@users.noreply.github.com>
2021-02-10 15:35:55 +0800

    Update flomo.sh

--- flomo.sh
+++ flomo.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
 
-/usr/local/bin/python3 flomo.py
-#--urikoappid $POPCLIP_OPTION_URIKOAPPID  --urikokey $POPCLIP_OPTION_URIKOKEY --urikofrom $POPCLIP_OPTION_URIKOFROM --urikoto $POPCLIP_OPTION_URIKOTO --urikoquery "$POPCLIP_TEXT"
+python3 flomo.py
